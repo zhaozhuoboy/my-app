9a1cb48c3c957558331916157022f36900d1a02b
zhaozhuoboy <zhaozhuoboy@163.com>
2016-09-20 18:11:59 +0800

    home

--- src/components/share/LeftNav.js
+++ src/components/share/LeftNav.js
@@ -5,11 +5,11 @@ class LeftNav extends React.Component {
   render () {
     return(
       <div className="left-nav">
-        <h3>ZHAOZHUO @ HOME</h3>
-        <Link activeClassName='active' onlyActiveOnIndex={true} to='/'><span className='glyphicon glyphicon-home' aria-hidden="true"></span> HOMT</Link>
-        <Link activeClassName='active' to='blog'><span className='glyphicon glyphicon-home' aria-hidden="true"></span> BLOG</Link>
-        <Link activeClassName='active' to='work'><span className='glyphicon glyphicon-home' aria-hidden="true"></span> WORK</Link>
-        <Link activeClassName='active' to='about'><span className='glyphicon glyphicon-home' aria-hidden="true"></span> ABOUT</Link>
+        <h3>ZHAOZHUO@HOME</h3>
+        <Link activeClassName='active' onlyActiveOnIndex={true} to='/'><span className='glyphicon glyphicon-home' aria-hidden="true"></span>&nbsp;&nbsp;HOME</Link>
+        <Link activeClassName='active' to='blog'><span className='glyphicon glyphicon-book' aria-hidden="true"></span>&nbsp;&nbsp;BLOG</Link>
+        <Link activeClassName='active' to='work'><span className='glyphicon glyphicon-tasks' aria-hidden="true"></span>&nbsp;&nbsp;WORK</Link>
+        <Link activeClassName='active' to='about'><span className='glyphicon glyphicon-user' aria-hidden="true"></span>&nbsp;&nbsp;ABOUT</Link>
       </div>
     )
   }
