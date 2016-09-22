6af59af77103f72b5d484dd69c0fdc406f853d9c
zhaozhuoboy <zhaozhuoboy@163.com>
2016-09-22 15:23:58 +0800

    blog finish

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,6 +1,7 @@
 import React, { PropTypes } from 'react';
 import BlogCard from './BlogCard';
 import axios from 'axios'
+import { getBlogList } from '../../utils/ajax.js'
 
 
 class Blog extends React.Component {
@@ -8,25 +9,28 @@ class Blog extends React.Component {
     super(props)
     this.state={
       blogData:[],
-      wite:true
+      wait:true
     }
   }
   componentDidMount(){
-    axios.get(`https://raw.githubusercontent.com/zhaozhuoboy/my-app-blog/master/index.json?${Math.random()}`)
-         .then( (res) => {
-           this.setState({
-             blogData:res.data,
-             wite:false
-           })
-         } )
+    getBlogList()
+    .then(
+      (recData) => {
+        this.setState({
+          blogData:recData.data,
+          wait:false
+        })
+      }
+    )
+
   }
   render () {
     return(
       <div style={{width:"100%",marginTop:"20px"}}>
-        {this.state.wite ?
-              <p style={{textAlign:"center",marginTop:"30%",fontSize:"18px"}}>
+        {this.state.wait ?
+              <p style={{textAlign:"center",marginTop:"28%",fontSize:"18px"}}>
                 <span className="glyphicon glyphicon-refresh" aria-hidden="true"
-                  style={{fontSize:"30px",animation:'loading 2s infinite'}}></span><br />
+                  style={{fontSize:"30px",animation:'loading 2s linear infinite'}}></span><br />
                 正在加载...
               </p> :
          this.state.blogData.map( (item,i) => <BlogCard {...item} key={i} />  )}
--- src/components/Blog/Item.js
+++ src/components/Blog/Item.js
@@ -1,10 +1,46 @@
-import React, { PropTypes } from 'react'
+import React, { PropTypes } from 'react';
+import marked from 'marked'
+import hljs from 'highlight.js';
+import '../../css/github.css';
+
+import { getMarkDown } from '../../utils/ajax.js'
 
 class Item extends React.Component {
+  constructor(){
+    super()
+    this.state={
+      md:'',
+      wait:true
+    }
+  }
+  componentDidMount(){
+    let mdName = this.props.params.url;
+
+    getMarkDown(mdName).then(
+      (recData) => {
+        this.setState({
+          md:recData.data,
+          wait:false
+        })
+      }
+    )
+  }
+  printMark(){
+    return {__html:marked(this.state.md)}
+  }
   render () {
     return(
-      <div>
-        {this.props.params.url}
+      <div style={{width:"100%"}}>
+        {
+          this.state.wait ?
+           <p style={{textAlign:"center",fontSize:"18px",color:"#20B2AA",marginTop:"28%"}}>
+            <span className="glyphicon glyphicon-refresh" aria-hidden="true"
+              style={{fontSize:"30px",animation:'loading 1s linear infinite'}}></span><br />
+            正在加载...
+          </p> :
+          <div className="blog-item-content" dangerouslySetInnerHTML={this.printMark()}/>
+        }
+
       </div>
     )
   }
