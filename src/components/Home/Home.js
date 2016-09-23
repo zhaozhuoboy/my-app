fd30d7ac4ee1dc6b0cd35a7b49026df4fc806d3e
zhaozhuoboy <zhaozhuoboy@163.com>
2016-09-23 08:54:10 +0800

    work page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,7 +9,7 @@ class Home extends React.Component {
             <div className="home-item">
               <h2 style={{fontSize:"40px",marginBottom:"20px"}}>HI,I'M <span style={{color:"#ffd800"}}>ZHAOZHUO</span></h2>
               <p style={{fontWeight:"100",letterSpacing:"3px"}}>WEB DEVELOPER</p>
-              <button><Link to='blog'>READ BLOG</Link></button>
+              <button className='read-btn'><Link to='blog'>READ BLOG</Link></button>
             </div>
           </div>
         </div>
--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -4,8 +4,51 @@ class Work extends React.Component {
   render () {
     return(
 
-      <div>
-        work
+      <div className='work'>
+        <div className="container-fluid">
+          <h2>我的作品</h2>
+          <div className="row">
+            <a href='#'>
+              <div className='col-xs-12 col-sm-6 col-md-4'>
+                <div className="work-card">
+                  <img src="http://7xopqp.com1.z0.glb.clouddn.com/contain3.jpg" alt=""/>
+                  <div className='work-desc'>
+                    <h4>作品一</h4>
+                    <p>这是第一个作品</p>
+                  </div>
+                </div>
+              </div>
+            </a>
+
+            <a href='#'>
+              <div className='col-xs-12 col-sm-6 col-md-4'>
+                <div className="work-card">
+                  <img src="http://7xopqp.com1.z0.glb.clouddn.com/contain3.jpg" alt=""/>
+                  <div className='work-desc'>
+                    <h4>作品一</h4>
+                    <p>这是第一个作品</p>
+                  </div>
+                </div>
+              </div>
+            </a>
+
+
+            <a href='#'>
+              <div className='col-xs-12 col-sm-6 col-md-4'>
+                <div className="work-card">
+                  <img src="http://7xopqp.com1.z0.glb.clouddn.com/contain3.jpg" alt=""/>
+                  <div className='work-desc'>
+                    <h4>作品一</h4>
+                    <p>这是第一个作品</p>
+                  </div>
+                </div>
+              </div>
+            </a>
+
+
+
+          </div>
+        </div>
       </div>
     )
   }
