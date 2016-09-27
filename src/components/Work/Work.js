181d2e9c141cc671858dbc2404da680feba332c3
zhaozhuoboy <zhaozhuoboy@163.com>
2016-09-27 11:22:03 +0800

    a

--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -1,4 +1,8 @@
-import React, { PropTypes } from 'react'
+import React, { PropTypes } from 'react';
+
+import freeline from '../../images/work/freeline.png'
+import weixin from '../../images/work/weixin.png'
+import waimai from '../../images/work/waimai.png'
 
 class Work extends React.Component {
   render () {
@@ -8,38 +12,38 @@ class Work extends React.Component {
         <div className="container-fluid">
           <h2>我的作品</h2>
           <div className="row">
-            <a href='#'>
+            <a href='http://freeline.coding.me/'>
               <div className='col-xs-12 col-sm-6 col-md-4'>
                 <div className="work-card">
-                  <img src="http://7xopqp.com1.z0.glb.clouddn.com/contain3.jpg" alt=""/>
+                  <img src={freeline} alt=""/>
                   <div className='work-desc'>
-                    <h4>作品一</h4>
-                    <p>这是第一个作品</p>
+                    <h4>FreeLine 漂移板社团</h4>
+                    <p>由于喜好自学HTML和CSS之后开始做这个网站，这个作品也获得了学校网页设计大赛的二等奖</p>
                   </div>
                 </div>
               </div>
             </a>
 
-            <a href='#'>
+            <a href='https://zhaozhuoboy.github.io/weixin-h5/'>
               <div className='col-xs-12 col-sm-6 col-md-4'>
                 <div className="work-card">
-                  <img src="http://7xopqp.com1.z0.glb.clouddn.com/contain3.jpg" alt=""/>
+                  <img src={weixin} alt=""/>
                   <div className='work-desc'>
-                    <h4>作品一</h4>
-                    <p>这是第一个作品</p>
+                    <h4>微信场景应用</h4>
+                    <p>使用swiper + zepto 制作的微信场景应用</p>
                   </div>
                 </div>
               </div>
             </a>
 
 
-            <a href='#'>
+            <a href='https://zhaozhuoboy.github.io/waimai/'>
               <div className='col-xs-12 col-sm-6 col-md-4'>
                 <div className="work-card">
-                  <img src="http://7xopqp.com1.z0.glb.clouddn.com/contain3.jpg" alt=""/>
+                  <img src={waimai} alt=""/>
                   <div className='work-desc'>
-                    <h4>作品一</h4>
-                    <p>这是第一个作品</p>
+                    <h4>外卖网</h4>
+                    <p>按照一家外卖网站，原样实现前台页面</p>
                   </div>
                 </div>
               </div>
