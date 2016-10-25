707f8a446650132dd2411d9b25df72be370573c6
zhaozhuoboy <zhaozhuoboy@163.com>
2016-10-25 13:15:55 +0800

    finish

--- src/components/share/Footer.js
+++ src/components/share/Footer.js
@@ -2,9 +2,35 @@ import React, { PropTypes } from 'react'
 
 class Footer extends React.Component {
   render () {
+    let styles={
+      root:{
+        width:"100%",
+        backgroundColor:"#111",
+      },
+      inner:{
+        width:"80%",
+        minWidth:"1000px",
+        margin:"0 auto",
+        padding:"20px 0",
+        color:"#fff",
+      },
+      p:{
+        fontSize:"18px",
+        textAlign:"center",
+      },
+      erweima:{
+        width:"150px",
+        margin:"0 auto"
+      }
+    }
     return(
-      <div>
-        footer
+      <div style={styles.root}>
+          <div style={styles.inner}>
+            <p style={styles.p}>欢迎加我的微信 : zhaozhuoboy</p>
+            <div style={styles.erweima}>
+              <img style={{width:"100%"}} src='http://obmtlh6zk.bkt.clouddn.com/image/erweimammqrcode1477369708398.png'/>
+            </div>
+          </div>
       </div>
     )
   }
